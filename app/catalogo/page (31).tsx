929f5d8eee8c199c2b9f34adc75f80ae941334b1
masoneriasantafesina <personal.grido739@gmail.com>
2026-09-14 12:05:27 -0300

    Add files via upload

---
+++ app/catalogo/page (31).tsx
@@ -0,0 +1,5 @@
+import CatalogoCliente from '@/components/CatalogoCliente';
+
+export default function CatalogoPage() {
+  return <CatalogoCliente marca="grido" nombreNegocio="Grido Store y Mayorista" />;
+}
